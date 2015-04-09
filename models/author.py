193f2dd5e3953cb42c6db602782319afab835bc3
Zack Zlotnik <zackzlotnik@gmail.com>
2015-04-09 23:01:25 +0000

    Added some helper methods to author.py and associated tests.

--- models/author.py
+++ models/author.py
@@ -15,3 +15,37 @@ class Author(base_model.BaseModel):
     self.username = username
     self.comments = OrderedDict()
     self.blogposts = OrderedDict()
+
+  def AddBlogPost(self, blogpost):
+    """Adds a blogpost to the author's dictionary of blogposts.
+
+    Args:
+      blogpost: BlogPost; the blog post to add.
+    """
+    if blogpost.id not in self.blogposts:
+      self.blogposts[blogpost.id] = blogpost
+
+  def AddComment(self, comment):
+    """Adds a comment to the author's dictionary of comments.
+
+    Args:
+      comment: Comment; the comment to add.
+    """
+    if comment.id not in self.comments:
+      self.comments[comment.id] = comment
+
+  def GetBlogPosts(self):
+    """Gets all blog posts for this user.
+
+    Returns:
+      A list of all blog posts for this user.
+    """
+    return self.blogposts.values()
+
+  def GetComments(self):
+    """Gets all comments for this user.
+
+    Returns:
+      A list of all comments for this user.
+    """
+    return self.comments.values()
--- models/author_test.py
+++ models/author_test.py
@@ -5,20 +5,100 @@ import author as author_model
 from collections import OrderedDict
 
 
+class BlogPost(object):
+  """Stub BlogPost class."""
+  def __init__(self):
+    self.id = id(self)
+
+
+class Comment(object):
+  """Stub Comment class."""
+  def __init__(self):
+    self.id = id(self)    
+
+
 class AuthorTests(unittest.TestCase):
   def setUp(self):
     self.username = 'zack'
+    self.blogpost = BlogPost()
+    self.comment = Comment()
 
   def tearDown(self):
     pass
 
   def testConstructor(self):
     author = author_model.Author(self.username)
+
     self.assertEquals(author.username, self.username)
     self.assertIsInstance(author.comments, OrderedDict)
     self.assertIsInstance(author.blogposts, OrderedDict)
     self.assertIsNotNone(author.created_timestamp)
     self.assertIsNotNone(author.id)
+
+  def testAddBlogPost_NewPost(self):
+    author = author_model.Author(self.username)
+    author.AddBlogPost(self.blogpost)
+
+    self.assertTrue(self.blogpost.id in author.blogposts)
+
+  def testAddBlogPost_ExistingPost(self):
+    author = author_model.Author(self.username)
+    author.AddBlogPost(self.blogpost)
+    author.AddBlogPost(self.blogpost)
+
+    self.assertTrue(self.blogpost.id in author.blogposts)
+    self.assertEqual(len(author.blogposts), 1)
+
+  def testAddBlogPost_MultiplePosts(self):
+    author = author_model.Author(self.username)
+
+    for unused_x in xrange(5):
+      blogpost = BlogPost()
+      author.AddBlogPost(blogpost)
+      self.assertTrue(blogpost.id in author.blogposts)
+
+    self.assertEquals(len(author.blogposts), 5)
+
+  def testAddComment_NewComment(self):
+    author = author_model.Author(self.username)
+    author.AddComment(self.comment)
+
+    self.assertTrue(self.comment.id in author.comments)
+
+  def testAddComment_ExistingComment(self):
+    author = author_model.Author(self.username)
+    author.AddComment(self.comment)
+    author.AddComment(self.comment)
+
+    self.assertEquals(len(author.comments), 1)
+    self.assertTrue(self.comment.id in author.comments)
+
+  def testAddComment_MultipleComments(self):
+    author = author_model.Author(self.username)
+
+    for unused_x in xrange(5):
+      comment = Comment()
+      author.AddComment(comment)
+      self.assertTrue(comment.id in author.comments)
+
+    self.assertEqual(len(author.comments), 5)
+
+  def testGetBlogPosts_NoBlogPosts(self):
+    author = author_model.Author(self.username)
+    result = author.GetBlogPosts()
+    expected = []
+
+    self.assertEquals(result, expected)
+
+  def testGetBlogPosts_WithBlogPosts(self):
+    author = author_model.Author(self.username)
+
+    blogposts = [BlogPost() for unused_x in xrange(5)]
+    for blogpost in blogposts:
+      author.AddBlogPost(blogpost)
+
+    result = author.GetBlogPosts()
+    self.assertEquals(result, blogposts) 
 
 if __name__ == '__main__':
   unittest.main()
