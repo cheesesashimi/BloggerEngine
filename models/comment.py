2ea406d6ecfd47b11db7404702ebaab3bb46b9f7
Zack Zlotnik <zackzlotnik@gmail.com>
2015-04-09 23:35:49 +0000

    Added basic versions of comment and label models.

--- models/comment.py
+++ models/comment.py
@@ -1,1 +1,10 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+import base_model
+
+class Comment(base_model.BaseModel):
+  def __init__(self, author, blogpost, comment):
+    super(Comment, self).__init__()
+    self.author = author
+    self.blogpost = blogpost
+    self.comment = comment
--- models/label.py
+++ models/label.py
@@ -1,1 +1,11 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+from collections import OrderedDict
+import base_model
+
+
+class Label(base_model.BaseModel):
+  def __init__(self, label):
+    super(Label, self).__init__()
+    self.label = label.lower()
+    self.blogposts = OrderedDict()
