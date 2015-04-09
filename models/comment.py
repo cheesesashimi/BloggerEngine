e8ab928092bfd0c42109e4d4d44c02284ef32762
Zack Zlotnik <zackzlotnik@gmail.com>
2015-04-09 23:54:14 +0000

    Initial commit of comment model and tests.

--- models/comment.py
+++ models/comment.py
@@ -3,8 +3,19 @@
 import base_model
 
 class Comment(base_model.BaseModel):
-  def __init__(self, author, blogpost, comment):
+  def __init__(self, author, blogpost, comment_text):
+    """Constructor.
+
+    Args:
+      author: Author; The author of this comment.
+      blogpost: BlogPost; The blogpost this comment belongs to.
+      comment_text: String; The comment text.
+    """
     super(Comment, self).__init__()
+
+    author.AddComment(self)
+    blogpost.AddComment(self)
+
     self.author = author
     self.blogpost = blogpost
-    self.comment = comment
+    self.comment_text = comment_text
--- models/comment_test.py
+++ models/comment_test.py
@@ -1,1 +1,31 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+import mock
+import unittest
+
+import author as author_model
+import blogpost as blogpost_model
+import comment as comment_model
+
+
+class CommentTest(unittest.TestCase):
+  def setUp(self):
+    self.author = mock.MagicMock(spec=author_model.Author)
+    self.blogpost = mock.MagicMock(spec=blogpost_model.BlogPost)
+    self.comment_text = 'The quick brown fox jumps over the lazy dog.'
+
+  def tearDown(self):
+    del self.author
+    del self.blogpost
+
+  def testConstructor(self):
+    comment = comment_model.Comment(self.author, self.blogpost, 
+                                    self.comment_text)
+    self.assertTrue(self.author.AddComment.called)
+    self.assertTrue(self.blogpost.AddComment.called)
+    self.assertEquals(comment.comment_text, self.comment_text)
+    self.assertIsNotNone(comment.created_timestamp)
+    self.assertIsNotNone(comment.id)
+
+if __name__ == '__main__':
+  unittest.main()
