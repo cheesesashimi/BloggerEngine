e1c41285aca3240e72c2dd037d39a24e8ac5d780
Zack Zlotnik <zackzlotnik@gmail.com>
2015-04-09 23:39:29 +0000

    Implemented blogpost.py and tests.

--- models/blogpost.py
+++ models/blogpost.py
@@ -1,1 +1,45 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+from collections import OrderedDict
+import base_model
+
+
+class BlogPost(base_model.BaseModel):
+  def __init__(self, author, headline, body):
+    """Constructor."""
+    super(BlogPost, self).__init__()
+
+    self.author = author
+    self.author.AddBlogPost(self)
+
+    self.headline = headline
+    self.body = body
+
+    self.comments = OrderedDict()
+    self.labels = OrderedDict()
+
+  def AddComment(self, comment):
+    """Adds a comment to this blog post.
+
+    Args:
+      comment: Comment; the comment to add.
+    """
+    if comment.id not in self.comments:
+      self.comments[comment.id] = comment
+
+  def AddLabel(self, label):
+    """Adds a label to this blog post.
+
+    Args:
+      label: Label; the label to add.
+    """
+    if label.label not in self.labels:
+      self.labels[label.label] = label
+
+  def GetComments(self):
+    """Gets all the comments associated with this blog post.
+
+    Returns:
+      A list of comments.
+    """
+    return self.comments.values()
--- models/blogpost_test.py
+++ models/blogpost_test.py
@@ -1,1 +1,112 @@
-#/usr/bin/python
+#!/usr/bin/python
+
+import unittest
+import mock
+
+import author as author_model
+import blogpost as blogpost_model
+import comment as comment_model
+import label as label_model
+
+from collections import OrderedDict
+
+
+class BlogPostTests(unittest.TestCase):
+  def setUp(self):
+    self.author = mock.MagicMock(spec=author_model.Author)
+    self.headline = 'Hello world!'
+    self.body = 'I\'m a blog post!'
+
+  def tearDown(self):
+    del self.author
+
+  def testConstructor(self):
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+
+    self.assertTrue(self.author.AddBlogPost.called)
+    self.assertEquals(self.headline, blogpost.headline)
+    self.assertEquals(self.body, blogpost.body)
+    self.assertIsInstance(blogpost.comments, OrderedDict)
+    self.assertIsInstance(blogpost.labels, OrderedDict)
+    self.assertIsNotNone(blogpost.created_timestamp)
+    self.assertIsNotNone(blogpost.id)
+
+  def testAddComment(self):
+    comment = mock.MagicMock(spec=comment_model.Comment)
+    comment.id = '12345'
+
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+    blogpost.AddComment(comment)
+
+    self.assertTrue(comment.id in blogpost.comments)
+
+  def testAddComment_MultipleComments(self):
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+    for comment in self.GenerateComments():
+      blogpost.AddComment(comment)
+      self.assertTrue(comment.id in blogpost.comments)
+
+    self.assertEquals(len(blogpost.comments), 5)
+
+  def testAddLabel(self):
+    label = mock.MagicMock(spec=label_model)
+    label.label = 'funny'
+
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+    blogpost.AddLabel(label)
+
+    self.assertTrue(label.label in blogpost.labels)
+
+  def testAddLabel_MultipleLabels(self):
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+
+    for label in self.GenerateLabels():
+      blogpost.AddLabel(label)
+      self.assertTrue(label.label in blogpost.labels)
+
+    self.assertEquals(len(blogpost.labels), 5)
+
+  def testGetComments_WithComments(self):
+    comment = mock.MagicMock(spec=comment_model.Comment)
+    comment.id = '12345'
+
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+    blogpost.AddComment(comment)
+
+    result = blogpost.GetComments()
+    expected = [comment]
+
+    self.assertEquals(result, expected)
+
+  def testGetComments_NoComments(self):
+    blogpost = blogpost_model.BlogPost(self.author, self.headline,
+                                       self.body)
+    result = blogpost.GetComments()
+    expected = []
+
+    self.assertEquals(result, expected)
+
+  def GenerateComments(self):
+    # I used a generator here since I needed to set the id property,
+    # otherwise, I would've just used a list comprehension.
+    for unused_x in xrange(5):
+      comment = mock.MagicMock(spec=comment_model.Comment)
+      comment.id = id(comment)
+      yield comment
+
+  def GenerateLabels(self):
+    # I used a generator here since I needed to set the id property,
+    # otherwise, I would've just used a list comprehension. 
+    for unused_x in xrange(5):
+      label = mock.MagicMock(spec=label_model.Label)
+      label.label = str(id(label))
+      yield label
+
+if __name__ == '__main__':
+  unittest.main()
