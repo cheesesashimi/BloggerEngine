5534573e54d38691da1e9ae99254d47e1b55c1da
Zack Zlotnik <zackzlotnik@gmail.com>
2015-04-11 02:35:52 +0000

    Added implementation of interface and associated tests.

--- interface.py
+++ interface.py
@@ -1,8 +1,14 @@
 #/usr/bin/python
 
+from models import author as author_model
+from models import blogpost as blogpost_model
+from models import comment as comment_model
+from models import label as label_model
+
 
 class BloggerEngine(object):
   """BloggerEngine interface."""
+
   def SubmitBlogPost(self, username, headline, body):
     """Submits a blog post.
 
@@ -10,8 +16,16 @@ class BloggerEngine(object):
       username: string; A string representation a post author's name.
       headline: string; A string representing the blog post's headline.
       body: string; A string representing the body of the blog post.
+
+    Returns:
+      A BlogPost instance with a nested Author instance.
     """
-    pass
+    author = self.GetOrInsertAuthor_(username)
+
+    post = blogpost_model.BlogPost(author, headline, body)
+    post.put()
+
+    return post
     
   def AddLabelToBlogPost(self, label_text, blogpost_id):
     """Adds a label to a given blog post.
@@ -19,8 +33,21 @@ class BloggerEngine(object):
     Args:
       label_text: string; The label text to add to a blog post.
       blogpost_id: string; The blog post ID to add the label to.
+
+    Returns:
+      A Label instance.
     """
-    pass
+    post = blogpost_model.BlogPost.GetByStorageKey(blogpost_id)
+    if not post:
+      return None
+
+    label = label_model.Label.GetByStorageKey(label_text)
+    if not label:
+      label = label_model.Label(label_text)
+      label.put()
+
+    label.AddToBlogPost(post)
+    return label
 
   def SubmitComment(self, username, comment_text, blogpost_id):
     """Submits a comment.
@@ -30,7 +57,15 @@ class BloggerEngine(object):
       comment_text: string; The comment text's content.
       blogpost_id: string; The blog post ID to add the comment to.
     """
-    pass
+    post = blogpost_model.BlogPost.GetByStorageKey(blogpost_id)
+    if not post:
+      return None
+
+    author = self.GetOrInsertAuthor_(username)
+    comment = comment_model.Comment(author, post, comment_text)
+    comment.put()
+
+    return comment 
 
   def GetCommentsByUsername(self, username):
     """Gets all comments for a given username.
@@ -39,9 +74,11 @@ class BloggerEngine(object):
       username: string; A string representing the comment author's username.
 
     Returns:
-      A list of that user's comments.
+      A list of that user's comments, if the author is found.
     """
-    pass
+    author = author_model.Author.GetByStorageKey(username)
+    if author:
+      return author.GetComments()
 
   def GetBlogPostsByUsername(self, username):
     """Gets all blog posts for a given username.
@@ -51,7 +88,9 @@ class BloggerEngine(object):
     Returns:
       A list of the given user's blogposts.
     """
-    pass
+    author = author_model.Author.GetByStorageKey(username)
+    if author:
+      return author.GetBlogPosts()
 
   def GetBlogPostById(self, blogpost_id):
     """Gets a specific blogpost by ID.
@@ -62,15 +101,53 @@ class BloggerEngine(object):
     Returns:
       The requested blogpost or None, if not found.
     """
-    pass
+    return blogpost_model.BlogPost.GetByStorageKey(blogpost_id)
 
   def GetBlogPostsByLabel(self, label_text):
     """Gets all blogposts with a given label attached to them.
 
     Args:
-      label_tset: string; The label text to query for.
+      label_text: string; The label text to query for.
 
     Returns:
       The requested blogposts or None, if not found.
     """
-    pass
+    label = label_model.Label.GetByStorageKey(label_text)
+    if label:
+      return label.GetBlogPosts()
+
+  def GetAllBlogPosts(self):
+    """Gets all blog posts.
+
+    Returns:
+      A list of blog posts.
+    """
+    return blogpost_model.BlogPost.GetAll()
+
+  def GetAuthorByUsername(self, username):
+    """Gets an author object by username.
+
+    Args:
+      username: String; The username to look up.
+
+    Returns:
+      A populated Author instance, if found.
+    """
+    return author_model.Author.GetByStorageKey(username)
+
+  def GetOrInsertAuthor_(self, username):
+    """Gets or inserts an Author object by username.
+
+    Args:
+      username: String; The username to look up.
+
+    Returns:
+      A populated Author instance.
+    """
+    author = author_model.Author.GetByStorageKey(username)
+    if author:
+      return author
+
+    author = author_model.Author(username)
+    author.put()
+    return author
--- interface_test.py
+++ interface_test.py
@@ -1,39 +1,245 @@
 #!/usr/bin/python
 
 import unittest
-import mock
 
 import interface
+
+from models import author as author_model
+from models import base_model
+from models import blogpost as blogpost_model
+from models import comment as comment_model
+from models import label as label_model
 
 
 class BloggerEngineTest(unittest.TestCase):
   def setUp(self):
-    pass
+    author_model.Author.instances = {}
+    base_model.BaseModel.instances = {}
+    blogpost_model.BlogPost.instances = {}  
+    comment_model.Comment.instances = {}
+    label_model.Label.instances = {}
+
+    colin = author_model.Author('colin')
+    colin.put()
+
+    zack = author_model.Author('zack')
+    zack.put()
+
+    blogpost1 = blogpost_model.BlogPost(zack,
+                                        'Hi!',
+                                        'Lorem ipsum...')
+    blogpost1.put()
+
+    blogpost2 = blogpost_model.BlogPost(zack,
+                                        'Caps lock',
+                                        'Not the caps lock!')
+    blogpost2.put()
+
+    blogpost3 = blogpost_model.BlogPost(colin,
+                                        'Hi!',
+                                        'Hello world.')
+    blogpost3.put()
+
+    label1 = label_model.Label('intro')
+    label1.put()
+    label1.AddToBlogPost(blogpost1)
+    label1.AddToBlogPost(blogpost3)
+
+    label2 = label_model.Label('funny')
+    label2.put()
+    label2.AddToBlogPost(blogpost2)
+
+    label3 = label_model.Label('good stuff')
+    label3.put()
+    label3.AddToBlogPost(blogpost1)
+    label3.AddToBlogPost(blogpost2)
+    label3.AddToBlogPost(blogpost3)
+
+    comment1 = comment_model.Comment(colin,
+                                     blogpost1,
+                                     '...et dolar.')
+    comment1.put()
+
+    comment2 = comment_model.Comment(colin,
+                                     blogpost2,
+                                     'Anything but the caps lock!')
+    comment2.put()
+
+    comment3 = comment_model.Comment(zack,
+                                     blogpost3,
+                                     'Welcome!')
+    comment3.put()
+
+    self.username = 'zack'
+    self.headline = 'Hello world!'
+    self.body = 'Yo!'
+    self.label_text = 'good'
+
+    self.authors = [colin, zack]
+    self.blogposts = [blogpost1, blogpost2, blogpost3]
+    self.comments = [comment1, comment2, comment3]
+    self.labels = [label1, label2, label3]
+
+    self.blogger_engine = interface.BloggerEngine()
 
   def tearDown(self):
-    pass
+    del self.blogger_engine
+    del self.username
+    del self.headline
+    del self.body
+    del self.label_text
+    del self.authors
+    del self.blogposts
+    del self.comments
+    del self.labels
 
   def testSubmitBlogPost(self):
-    pass
+    result = self.blogger_engine.SubmitBlogPost(self.username,
+                                                self.headline,
+                                                self.body)
 
-  def testAddLabelToBlogPost(self):
-    pass
+    self.assertIsInstance(result, blogpost_model.BlogPost)
+    self.assertIsInstance(result.author, author_model.Author)
+    self.assertEquals(result.author.username, self.username)
+    self.assertEquals(result.headline, self.headline)
+    self.assertEquals(result.body, self.body)
 
-  def testSubmitCommit(self):
-    pass
+    self.assertEquals(len(blogpost_model.BlogPost.instances['BlogPost']), 4)
 
-  def testGetCommentsByUsername(self):
-    pass
+  def testAddLabelToBlogPost_BlogPostFound(self):
+    blogpost = self.blogger_engine.SubmitBlogPost(self.username,
+                                                  self.headline,
+                                                  self.body)
 
-  def testGetBlogPostsByUsername(self):
-    pass
+    result = self.blogger_engine.AddLabelToBlogPost(self.label_text,
+                                                    blogpost.id)
 
-  def testGetBlogPostById(self):
-    pass
+    self.assertIsInstance(result, label_model.Label)
+    self.assertEquals(result.label, self.label_text)
 
-  def testGetBlogPostsByLabel(self):
-    pass
+    self.assertEquals(len(label_model.Label.instances['Label']), 4)
 
+  def testAddLabelToBlogPost_BlogPostNotFound(self):
+    result = self.blogger_engine.AddLabelToBlogPost(self.label_text, '12345')
+
+    self.assertIsNone(result)
+
+  def testSubmitComment_BlogPostFound(self):
+    blogpost = self.blogger_engine.SubmitBlogPost(self.username,
+                                                  self.headline,
+                                                  self.body)
+
+    comment_author = 'colin'
+    comment_text = 'Anything but the caps lock!'
+
+    result = self. blogger_engine.SubmitComment(comment_author,
+                                                comment_text,
+                                                blogpost.id)
+
+    self.assertIsInstance(result, comment_model.Comment)
+    self.assertEquals(result.comment_text, comment_text)
+    self.assertIsInstance(result.author, author_model.Author)
+    self.assertEquals(result.author.username, comment_author)
+
+  def testSubmitComment_BlogPostNotFound(self):
+    comment_author = 'colin'
+    comment_text = 'Anything but the caps lock!'
+    blogpost_id = '12345'
+
+    result = self.blogger_engine.SubmitComment(comment_author,
+                                               comment_text,
+                                               blogpost_id)
+
+    self.assertIsNone(result)
+   
+  def testGetCommentsByUsername_UserFound(self):
+    result = self.blogger_engine.GetCommentsByUsername('colin')
+    expected = [self.comments[0], self.comments[1]]
+
+    self.assertEquals(result, expected)
+     
+  def testGetCommentsByUsername_UserNotFound(self):
+    result = self.blogger_engine.GetCommentsByUsername('steve')
+    self.assertIsNone(result)
+ 
+  def testGetCommentsByUsername_UserFoundNoComments(self):
+    author_without_comments = author_model.Author('jon')
+    author_without_comments.put()
+
+    result = self.blogger_engine.GetCommentsByUsername('jon')
+    expected = []
+
+    self.assertEquals(result, expected)
+  
+  def testGetBlogPostsByUsername_UsernameFound(self):
+    result = self.blogger_engine.GetBlogPostsByUsername('colin')
+    expected = [self.blogposts[2]]
+    self.assertEquals(result, expected)
+    
+  def testGetBlogPostsByUsername_UsernameNotFound(self):
+    result = self.blogger_engine.GetBlogPostsByUsername('steve')
+    self.assertIsNone(result)
+
+  def testGetBlogPostsByUsername_UsernameFoundNoPosts(self):
+    author_without_posts = author_model.Author('jon')
+    author_without_posts.put()
+
+    result = self.blogger_engine.GetBlogPostsByUsername('jon')
+    expected = []
+    self.assertEquals(result, expected)
+
+  def testGetBlogPostById_BlogPostIdFound(self):
+    for blogpost in self.blogposts:
+      result = self.blogger_engine.GetBlogPostById(blogpost.id)
+      expected = blogpost
+      self.assertEquals(result, expected)
+
+  def testGetBlogPostById_NoBlogPostFound(self):
+    result = self.blogger_engine.GetBlogPostById('12345')
+    self.assertIsNone(result)
+
+  def testGetBlogPostsByLabel_LabelExists(self):
+    result = self.blogger_engine.GetBlogPostsByLabel('funny')
+    expected = [self.blogposts[1]]
+    self.assertEquals(result, expected)
+    
+    result = self.blogger_engine.GetBlogPostsByLabel('intro')
+    expected = [self.blogposts[0], self.blogposts[2]]
+    self.assertEquals(result, expected)
+
+    result = self.blogger_engine.GetBlogPostsByLabel('good stuff')
+    expected = self.blogposts
+    self.assertEquals(result, expected)
+
+  def testGetBlogPostsByLabel_LabelDoesNotExist(self):
+    result = self.blogger_engine.GetBlogPostsByLabel('nothing')
+    self.assertIsNone(result)
+
+  def testGetOrInsertAuthor_AuthorFound(self):
+    result = self.blogger_engine.GetOrInsertAuthor_('zack')
+    expected = self.authors[1]
+    self.assertEquals(result, expected)
+    self.assertEquals(len(author_model.Author.instances['Author']), 2)
+
+  def testGetOrInsertAuthor_AuthorNotFound(self):
+    result = self.blogger_engine.GetOrInsertAuthor_('jon')
+    self.assertEquals(result.username, 'jon')
+    self.assertIsInstance(result, author_model.Author)
+    self.assertEquals(len(author_model.Author.instances['Author']), 3)
+
+  def testGetAuthorByUsername_AuthorFound(self):
+    result = self.blogger_engine.GetAuthorByUsername('zack')
+    expected = self.authors[1]
+    self.assertEquals(result, expected)
+
+  def testGetAuthorByUsername_AuthorNotFound(self):
+    result = self.blogger_engine.GetAuthorByUsername('steve')
+    self.assertIsNone(result)
+
+  def testGetAllBlogPosts(self):
+    result = self.blogger_engine.GetAllBlogPosts()
+    expected = self.blogposts
+    self.assertEquals(result, expected)
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
